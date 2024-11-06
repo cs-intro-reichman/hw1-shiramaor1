7247e0ba83fef153f0362b9d5bdc4a9baf85ec34
שירה מאור <shira.maor@post.runi.ac.il>
2024-11-06 21:05:58 +0200

    .\NumWords task

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,10 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int currentVal = Integer.parseInt(args[0]);
+		int percent = Integer.parseInt(args[1]);
+		int yrs = Integer.parseInt(args[2]);
+		Double futureVal = Math.pow(cv*(percent+1) 
+
 	}
 }
