22ab1d7d9a5ed7d34ed861d544d72ad58fe99188
שירה מאור <shira.maor@post.runi.ac.il>
2024-11-06 18:50:49 +0200

    Bill3 task

--- Bill3.java
+++ Bill3.java
@@ -3,10 +3,11 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-		String name2 = args[0];
-		String name3 = args[0];
-	    int Bill = Integer.parseInt(args[0]);
-		double eachamount  = Double.parseDouble(args[0]);
-		System.out.println("Hello World");
+		String name2 = args[1];
+		String name3 = args[2];
+	    double totalAmount = Double.parseDouble(args[3]);
+		double eachAmount = Math.ceil(totalAmount/3);
+		System.out.println("dear " + name1 + ", " + name2 + ", and " + name3
+		                    + ": pay " + eachAmount + " shekels each " );
 	}
 }
