ebf8c7e41c71c65ac929c2590647702ef46b6e62
שירה מאור <shira.maor@post.runi.ac.il>
2024-11-07 17:18:47 +0200

    .\NumWords program

--- NumWords.java
+++ NumWords.java
@@ -5,7 +5,7 @@ public class NumWords {
 	    int hundreds = number / 100;
 		int tens = number % 100 / 10; 
 		int ones = number % 10;
-		System.out.println(hundreds + " hundreds, " + tens + " tens, " + ones + " ones.");
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 
 	}
 }
